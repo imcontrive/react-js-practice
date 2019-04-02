19dc30c5a0c41b6d438fd07717c5b68145b7dab1
shubh <psubham94@gmail.com>
2019-04-02 21:26:00 +0530

    add cart-items

--- Shopping_Cart/src/App.js
+++ Shopping_Cart/src/App.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import Login from './components/Login';
-import User from './components/User';
+
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      user: {
-        firstName : "Ankit",
-        lastName : "Sinha",
-        img: `https://pbs.twimg.com/profile_images/1019277956286304256/DBOt4zgP_400x400.jpg`
-      }
+      items: null
     }
   }
-
+  componentDidMount = () => {
+    fetch(`https://react-shopping-cart-67954.firebaseio.com/products.json`).
+  }
+  
  
   render() {
     const { user } = this.state;
---
+++ Shopping_Cart/src/components/CartItems.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CartItems extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
